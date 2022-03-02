d955e7a571d543ae73855e2c9acaeff0c4f49813
devexpert9 <18286410+devexpert9@users.noreply.github.com>
2022-03-02 16:25:27 +0530

    update

---
+++ index.js
@@ -0,0 +1,25 @@
+let app = require("express")();
+let http = require("http").Server(app);
+let io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(" socket connection established...");
+  socket.on("send_message", (result) => {
+    console.log("new send_messagehhhhhhhhhhhhhhhhhhhhhhhhhhhh");
+    io.emit("send_message", result);
+  });
+  socket.on("online", (result) => {
+    console.log("online");
+    io.emit("online", result);
+  });
+});
+
+var port = process.env.PORT || 3002;
+
+http.listen(port, function () {
+  console.log("socket running on port :" + port);
+});
